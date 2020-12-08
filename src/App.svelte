eac4bcc05331f823b3787bc369622945d6ec6193
SilentEchoUK <silentechouk@gmail.com>
2020-12-09 00:39:59 +0100

    initial

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Chevron from "./Chevron.svelte";
+
+  let boost = 100;
+  $: boost = boost;
+</script>
+
+<input type="number" bind:value={boost} />
+
+<Chevron {boost} />
---
+++ src/Chevron.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let boost;
+  let src;
+  $: boost = Math.round(boost / 10) * 10;
+  $: src = `/chevron/chevron_${boost} boost.png`;
+
+  $: console.log(src);
+</script>
+
+<img {src} width="69" height="69" alt="" />
